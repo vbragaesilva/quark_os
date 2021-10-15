6d83e7e7ff3dc665a32cb46c1b013ea2fb07661f
vbragaesilva <vbs.vino@gmail.com>
2021-10-15 18:05:29 +0000

    works but gitignore __elf

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -4,5 +4,16 @@ section .text
 bits 32
 start:
     ;Print 'OK'
+    ;mov ah, 0x0e
+    ;mov al, 'H'
+    ;int 0x10
+
     mov dword [0xb8000], 0x2f4b2f4f
+
     hlt
+    jmp $
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+
+
